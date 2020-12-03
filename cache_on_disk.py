3b3d5820d32ded36b16931867fa96c00f9ebd960
路志远 <1551755561@qq.com>
2020-12-03 18:24:28 +0800

    improvement: 简化 cache_key 实现；重构代码

--- cache_on_disk.py
+++ cache_on_disk.py
@@ -3,53 +3,32 @@ import functools
 import os
 import time
 import io
-from typing import Any
 
 NORMALIZABLE_TYPES = (str, int, float, bool)
-CACHE_FILE_DIR = ".cache"
-
-if not os.path.exists(CACHE_FILE_DIR):
-    os.mkdir(CACHE_FILE_DIR)
+DEFAULT_CACHE_FILE_DIR = ".cache"
 
 
-def _normalize_cache_key(v):
-    if isinstance(v, type):
-        return v.__name__
-    if isinstance(v, bytes):
-        return v.decode()
-    if isinstance(v, (list, set)):
-        return "-".join(map(_normalize_cache_key, v))
-    if v is None or isinstance(v, NORMALIZABLE_TYPES):
-        return str(v)
-    raise ValueError(f"Only support cache keys of types {[t.__name__ for t in NORMALIZABLE_TYPES]}")
-
-
-def _make_key(func, *args, **kwargs):
-    keys = [_normalize_cache_key(v) for v in args]
-    keys += sorted((f"{k}={_normalize_cache_key(v)}" for k, v in kwargs.items()))
-    return f"{func.__module__}.{func.__name__}.{'.'.join(keys)}"
-
-
-def _serialization(data) -> str:
-    return json.dumps(data)
-
-
-def _deserialization(file: io.TextIOWrapper):
-    return json.load(file)
+if not os.path.exists(DEFAULT_CACHE_FILE_DIR):
+    os.mkdir(DEFAULT_CACHE_FILE_DIR)
 
 
 class Cacher:
     """
     直接把返回值缓存在磁盘中，超方便！
 
+    Args:
+        timeout: 可选参数，默认 5*60 秒
+        cache_file_path: 可选参数，保存缓存文件的绝对路径
+
     Usage:
-        @Cacher(timeout=60)
+        @Cacher(timeout=5*60, cache_file_path="/Users/bytedance/Desktop/xxx.cache")
         def fun(a, b, c):
             return a+b+c
     """
 
-    def __init__(self, timeout=60*5):
+    def __init__(self, timeout=5*60, cache_file_path: str=""):
         self.timeout = timeout
+        self.custom_cache_file_path = cache_file_path
 
     def __call__(self, *args, **kwargs):
         func = args[0]
@@ -58,8 +37,8 @@ class Cacher:
     def decorator(self, func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            cahe_key = _make_key(func, *args, **kwargs)
-            cache_file_path = os.path.join(CACHE_FILE_DIR, cahe_key)
+            cahe_key = Cacher.make_key(func, *args, **kwargs)
+            cache_file_path = self.custom_cache_file_path or os.path.join(DEFAULT_CACHE_FILE_DIR, cahe_key)
             value = self.read_from_disk(cache_file_path)
             if not value:
                 value = func(*args, **kwargs)
@@ -73,11 +52,23 @@ class Cacher:
             file_expiration = file_created_at + self.timeout
             if file_expiration > time.time():
                 with open(cache_file_path) as file:
-                    return _deserialization(file)
+                    return Cacher.deserialization(file)
             else:
                 os.remove(cache_file_path)
         return None
 
-    def save_in_disk(self, cache_file_path: str, data: Any) -> None:
+    def save_in_disk(self, cache_file_path: str, data) -> None:
         with open(cache_file_path, 'w') as file:
-            file.write(_serialization(data))
+            file.write(Cacher.serialization(data))
+
+    @staticmethod
+    def make_key(func, *args, **kwargs):
+        return f"{func.__module__}.{func.__qualname__}.{str(args)}.{str(kwargs)}"
+
+    @staticmethod
+    def serialization(data) -> str:
+        return json.dumps(data)
+
+    @staticmethod
+    def deserialization(file: io.TextIOWrapper):
+        return json.load(file)
